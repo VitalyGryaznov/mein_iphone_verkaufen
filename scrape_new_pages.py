d533fdb06b8fa31b8da97b8792119f7e73c087b2
vitaly-gryaznov <Vitaly.Gryaznov@esailors.de>
2020-09-14 20:08:36 +0000

    update searching new pages

--- scrape_new_pages.py
+++ scrape_new_pages.py
@@ -2,12 +2,12 @@
 # -*- coding: utf-8 -*-
 
 # Saving new listings
-# do it in parallel for different devices
+# do it in parallel for different searcj=h terms
 
 import actions.actions as actions
 import helpers.data_helper as data_helper
+from joblib import Parallel, delayed
 
 products = data_helper.get_products()
 
-actions.scrape_new_listings(products[0])
-#Parallel(n_jobs=8, verbose=1, backend="threading")(map(delayed(scrape_one_page), page_numbers))
+Parallel(n_jobs=4, verbose=1, backend='loky')(map(delayed(actions.scrape_new_listings), products))
